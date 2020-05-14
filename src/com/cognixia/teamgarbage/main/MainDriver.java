7383ff85a7ec24222059231812f189b42156cd2c
ClarenceBumanglag <c.bumanglag.15@gmail.com>
2020-05-14 15:51:19 -0500

    Initial commit

---
+++ src/com/cognixia/teamgarbage/main/MainDriver.java
@@ -0,0 +1,15 @@
+package com.cognixia.teamgarbage.main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MainDriver {
+
+    public static void main(String args[]) {
+        Scanner userIn = new Scanner(System.in);
+
+
+    }
+}
---
+++ src/com/cognixia/teamgarbage/main/UserPrompter.java
@@ -0,0 +1,5 @@
+package com.cognixia.teamgarbage.main;
+
+public class UserPrompter {
+
+}
